ca943fcdf8c1aa9f18e7a815978dc464a3480003
Volodymyr Mukha <volodymyrmukha@gmail.com>
2012-03-09 14:46:39 +0200

    create resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '1de22c1bb88b2cea446ec25a6a8c552808f9d703978153b6b19d0d650c577d80cc7966b00afd369acc715fedb31e2c9c77a56040bf51204754e6143f127b3b19'
